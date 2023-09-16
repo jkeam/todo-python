b23516fe2a9bd0aad045c30858228f1e057a68bc
Jon Keam <jpkeam@gmail.com>
2023-09-16 01:42:09 -0400

    update openapi spec

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 import requests
 
 app = FastAPI()
@@ -17,3 +18,21 @@ def todos() -> dict:
 def todo(todo_id: int) -> dict:
     res = requests.get(f"{ROOT_URL}/{todo_id}").json()
     return {"data": res}
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Todo API",
+        version="3.0.0",
+        summary="Gets todos",
+        description="Awesome app that gets todos.",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://png.pngtree.com/png-vector/20190930/ourmid/pngtree-to-do-list-icon-cartoon-style-png-image_1768114.jpg"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
